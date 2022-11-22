b42ffdaf577fac1bdae979ce5fd264f9a58d8ed9
Nikita <windownik@gmail.com>
2022-11-22 16:19:12 +0300

    Try add swagger

--- bin/server.dart
+++ bin/server.dart
@@ -1,27 +1,47 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_swagger_ui/shelf_swagger_ui.dart';
 
 import 'sql.dart';
+
+// Handler swaggerHandler() {
+//   final swagger = SwaggerUI(
+//     'swagger.yaml',
+//     title: 'Swagger Test',
+//   );
+//   return swagger;
+// }
 
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
   ..get('/echo/<message>', _echoHandler)
+  // ..get('/doc', _doc)
   ..post('/create_db', _createDb);
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
 }
 
+// FutureOr<Response> _doc(Request req) {
+//   final swagger = SwaggerUI(
+//     'specs/swagger.yaml',
+//     title: 'Swagger Test',
+//   );
+//   return swagger.call(req);
+// }
+
 Future<Response> _createDb(Request request) async {
   DataB dataB = DataB();
   await dataB.createTables();
-  return Response.ok('Database Created!');
+  return Response.ok(jsonEncode(
+      {'status': true, 'message': 'Database Created!'}));
 }
-
 
 Future<Response> _echoHandler(Request request) async {
   dynamic message = request.params['message'];
@@ -32,14 +52,16 @@ Future<Response> _echoHandler(Request request) async {
   for (var i in info) {
     print(i);
   }
-  return Response.ok('$info\n');
+  return Response.ok(jsonEncode({'status': true, 'data': 123}));
 }
 
 void main(List<String> args) async {
+  // final path = 'specs/swagger.yaml';
+  // final handler = swaggerHandler();
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
+  // // Configure a pipeline that logs requests.
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
--- bin/sql.dart
+++ bin/sql.dart
@@ -14,10 +14,17 @@ class DataB {
      first_name TEXT DEFAULT '0',
      last_name TEXT DEFAULT '0',
      language TEXT DEFAULT '0',
+     push_token TEXT DEFAULT '0',
      status INTEGER DEFAULT 0,
      last_active timestamp',
      registrate_date timestamp'
      )''');
+    await connection.query('''CREATE TABLE IF NOT EXISTS auth (
+     id SERIAL PRIMARY KEY,
+     user_id INTEGER DEFAULT 0,
+     access_token TEXT DEFAULT '0',
+     refresh_token TEXT DEFAULT '0',
+     create_date timestamp)''');
     await connection.query('''CREATE TABLE IF NOT EXISTS chat (
      id SERIAL PRIMARY KEY,
      owner_id INTEGER DEFAULT 0,
@@ -27,12 +34,14 @@ class DataB {
     await connection.query('''CREATE TABLE IF NOT EXISTS users_in_chat (
      id SERIAL PRIMARY KEY,
      chat_id INTEGER DEFAULT 0,
-     user_id INTEGER DEFAULT 0)''');
+     user_id INTEGER DEFAULT 0,
+     connect_date timestamp)''');
     await connection.query('''CREATE TABLE IF NOT EXISTS message (
      id SERIAL PRIMARY KEY,
-     user_id INTEGER DEFAULT 0,
+     from_user INTEGER DEFAULT 0,
+     to_user INTEGER DEFAULT 0,
      chat_id INTEGER DEFAULT 0,
-     text INTEGER DEFAULT 0,
+     text TEXT DEFAULT 0,
      status TEXT DEFAULT 'create',
      create_date timestamp')''');
     return "ok";
